c522c82ba784f9968b2de403bdb5de4080d40a50
lau <kwlau1993@gmail.com>
2019-11-28 22:51:33 +0100

    Add reproducing Augmento's indicators

---
+++ Augmento/Basic_Startegy_v2.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 14 15:54:34 2019
+
+@author: raghav, lau
+
+#Largely based on Augmento's code at https://github.com/augmento-ai/quant-reseach
+"""
+
+#import sys
+import requests
+import datetime
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as md
+from pandas.io.json import json_normalize
+import pandas as pd
+
+import analysis_helper as ah
+
+# define the urls of the endpoints for topics
+topics_endpoint_url = "http://api-dev.augmento.ai/v0.1/topics"  
+
+# save a list of the augmento topics
+r_topic = requests.request("GET", topics_endpoint_url, timeout=10)
+
+temp_topic_data= r_topic.json()
+
+data_topic = json_normalize(temp_topic_data).T
+data_topic.columns = ['Labels']
+
+bearish_index= data_topic.index[data_topic['Labels'] == 'Bearish'].values
+positive_index= data_topic.index[data_topic['Labels'] == 'Positive'].values
+
+# define the url of the endpoint to get event data
+endpoint_url = "http://api-dev.augmento.ai/v0.1/events/aggregated"
+
+# define the start and end times
+datetime_start = datetime.datetime(2017, 1, 1)
+datetime_end = datetime.datetime.now()
+        
+# initialise a store for the data we're downloading
+sentiment_data = pd.DataFrame()
+
+# define a start pointer to track multiple requests
+start_ptr = 0
+count_ptr = 1000
+
+# get the data
+while start_ptr >= 0:
+    headers = {"api-key" : "LWn2e7VeXDtoANEvSgYZ"}
+	# define the parameters of the request
+    params = {
+        "source" : "twitter",
+		"coin" : "cardano",
+		"bin_size" : "1H",
+		"count_ptr" : count_ptr,
+		"start_ptr" : start_ptr,
+		"start_datetime" : datetime_start.strftime("%Y-%m-%dT%H:%M:%SZ"),
+		"end_datetime" : datetime_end.strftime("%Y-%m-%dT%H:%M:%SZ")
+	}
+	
+	# make the request
+    r = requests.request("GET", endpoint_url, params=params, headers=headers, timeout=10)
+	
+	# if the request was ok, add the data and increment the start_ptr
+	# else return an error
+    if r.status_code == 200:
+       temp_data = r.json()
+       if len(temp_data) == 0:
+           start_ptr = -1
+       else:
+           data = json_normalize(temp_data)
+           #data['datetime'] = str(data['datetime'])
+           labels = data['counts'].apply(pd.Series)
+           # rename each variable is tags
+           data_topic.index[data_topic['Labels'] == 'Bearish'].values
+           labels = labels.rename(columns = lambda x :temp_topic_data[str(x)])
+           data = pd.concat([data[:], labels[:]], axis=1)
+           tmp = data.drop('counts',axis=1)
+           #final_data = data[['Label_'+''.join(bearish_index),'Label_'+''.join(positive_index),'datetime','t_epoch']]
+           #sentiment_data=sentiment_data.append(tmp)
+           sentiment_data=sentiment_data.append(tmp)
+           start_ptr += count_ptr
+    else:
+       raise Exception("api call failed with status_code {:d}".format(r.status_code))
+    
+    # if we didn't get any data, assume we've got all the data
+    if len(temp_data) == 0:
+       start_ptr = -1
+    
+    # sleep
+    time.sleep(2.0)
+    
+    sentiment_data.to_csv('ADArd_2017_to_now.csv')
+
+aaa = sentiment_data[['Label_'+''.join(positive_index)]].values.tolist()
+aug_signal_a = np.array([float(i) for sublist in aaa for i in sublist])
+bbb = sentiment_data[['Label_'+''.join(bearish_index)]].values.tolist()
+aug_signal_b = np.array([float(i) for sublist in bbb for i in sublist])
+ttt = sentiment_data[['t_epoch']].values.tolist()
+t_aug_data = [int(i) for sublist in ttt for i in sublist]
+
+window_size_A = 28
+window_size_B = 14
+
+# generate the sentiment score
+sent_score = ah.nb_calc_sentiment_score_a(aug_signal_a, aug_signal_b, window_size_A, window_size_B)
+#sent_score = ah.nb_calc_sentiment_score_c(aug_signal_a, aug_signal_b, window_size, window_size)
+
+#notification signals based on sent_score
+
+notification = 'neutral'
+
+if sent_score[-1] > 0.0:
+    notification = 'long'
+elif sent_score[-1] <= 0.0:
+	notification = 'short'
+elif sent_score[-1] == 0.0:
+	notification = 'neutral'
+    
+print(notification)
+
+# set up the figure
+fig, ax = plt.subplots(3, 1, sharex=True, sharey=False)
+
+# initialise some labels for the plot
+datenum_aug_data = [md.date2num(datetime.datetime.fromtimestamp(el)) for el in t_aug_data]
+#datenum_price_data = [md.date2num(datetime.datetime.fromtimestamp(el)) for el in t_price_data]
+
+# plot stuff
+#ax[0].grid(linewidth=0.4)
+#ax[1].grid(linewidth=0.4)
+#ax[2].grid(linewidth=0.4)
+#ax[0].plot(datenum_price_data, price_data, linewidth=0.5)
+#ax[1].plot(datenum_aug_data, sent_score, linewidth=0.5)
+#ax[2].plot(datenum_price_data, pnl, linewidth=0.5)
+
+# label axes
+#ax[0].set_ylabel("Price")
+#ax[1].set_ylabel("Seniment score")
+#ax[2].set_ylabel("PnL")
+#ax[1].set_ylim([-5.5, 5.5])
+
+#ax[0].set_title("4_basic_strategy_example.py")
+
+# generate the time axes
+#plt.subplots_adjust(bottom=0.2)
+#plt.xticks( rotation=25 )
+#ax[0]=plt.gca()
+#xfmt = md.DateFormatter('%Y-%m-%d')
+#ax[0].xaxis.set_major_formatter(xfmt)
+
+# show the plot
+#plt.show()
---
+++ Augmento/analysis_helper.py
@@ -0,0 +1,231 @@
+import numpy as np
+import numba as nb
+
+
+@nb.jit("(f8[:])(f8[:], f8[:])", nopython=True, nogil=True, cache=True)
+def nb_safe_divide(a, b):
+	# divide each element in a by each element in b
+	# if element b == 0.0, return element = 0.0
+	c = np.zeros(a.shape[0], dtype=np.float64)
+	for i in range(a.shape[0]):
+		if b[i] != 0.0:
+			c[i] = a[i] / b[i]
+	return c
+
+@nb.jit("(f8[:])(f8[:], i8)", nopython=True, nogil=True, parallel=True)
+def nb_causal_rolling_average(arr, window_size):
+	
+	# create an output array
+	out_arr = np.zeros(arr.shape[0])
+	
+	# create an array from the input array, with added space for the rolling window
+	new_arr = np.hstack((np.ones(window_size-1) * arr[0], arr))
+	
+	# for each output element, find the mean of the last few input elements
+	for i in nb.prange(out_arr.shape[0]):
+		out_arr[i] = np.mean(new_arr[i : i + window_size])
+	
+	return out_arr
+
+@nb.jit("(f8[:])(f8[:], i8)", nopython=True, nogil=True, parallel=True)
+def nb_causal_rolling_sd(arr, window_size):
+	
+	# create an output array
+	out_arr = np.zeros(arr.shape[0])
+	
+	# create an array from the input array, with added space for the rolling window
+	new_arr = np.hstack((np.ones(window_size-1) * arr[0], arr))
+	
+	# for each output element, find the mean and std of the last few
+	# input elements, and standardise the input element by the mean and std of the window
+	for i in nb.prange(out_arr.shape[0]):
+		num = new_arr[i+window_size-1] - np.mean(new_arr[i : i + window_size-1])
+		denom = np.std(new_arr[i : i + window_size-1])
+		if denom != 0.0:
+			out_arr[i] = num / denom
+	
+	return out_arr
+
+#@nb.jit("(f8[:])(f8[:], i8, i8)", nopython=True, nogil=True, parallel=True)
+def nb_causal_rolling_sd_rand(arr, window_size_rand, window_size_std):
+
+	# create an output array               
+	out_arr = np.zeros(arr.shape[0])
+			
+	# create an array from the input array, with added space for the rolling window
+	new_arr = np.hstack((np.ones(window_size_rand-1) * arr[0], arr))
+	
+	# create an array from the input array, with added space for the rolling window
+	new_arr = np.hstack((np.ones(window_size_rand-1) * arr[0], arr))
+	# for each output element, find the mean and std of the last few
+	# input elements, and standardise the input element by the mean and std of the window
+	for i in nb.prange(out_arr.shape[0]):
+			window_size = round(np.random.normal(window_size_rand, window_size_std))       
+			num = new_arr[i+window_size-1] - np.mean(new_arr[i : i + window_size-1])
+			denom = np.std(new_arr[i : i + window_size-1])
+			if denom != 0.0:
+					out_arr[i] = num / denom
+
+	return out_arr
+
+@nb.jit("(f8[:])(f8[:], i8)", nopython=True, nogil=True, parallel=True)
+def nb_causal_rolling_norm(arr, window_size):
+	
+	# create an output array
+	out_arr = np.zeros(arr.shape[0])
+	
+	# create an array from the input array, with added space for the rolling window
+	new_arr = np.hstack((np.ones(window_size-1) * arr[0], arr))
+	
+	# for each output element, find the mean and std of the last few
+	# input elements, and standardise the input element by the mean and std of the window
+	for i in nb.prange(out_arr.shape[0]):
+			num = new_arr[i+window_size-1] - np.mean(new_arr[i : i + window_size])
+			denom = np.max(np.abs(new_arr[i : i + window_size] - np.mean(new_arr[i : i + window_size])))
+			if denom != 0.0:
+					out_arr[i] = num / denom
+	
+	return out_arr
+	
+@nb.jit("(f8[:])(f8[:], i8, i8)", nopython=True, nogil=True, parallel=True)
+def nb_causal_rolling_norm_rand(arr, window_size_rand, window_size_std):
+	   
+	# create an output array
+	out_arr = np.zeros(arr.shape[0])
+	
+	# create an array from the input array, with added space for the rolling window
+	new_arr = np.hstack((np.ones(window_size_rand-1) * arr[0], arr))
+	
+	# for each output element, find the mean and std of the last few
+	# input elements, and standardise the input element by the mean and std of the window
+	for i in nb.prange(out_arr.shape[0]):
+			window_size = round(np.random.normal(window_size_rand, window_size_std))
+			num = new_arr[i+window_size-1] - np.mean(new_arr[i : i + window_size])
+			denom = np.max(np.abs(new_arr[i : i + window_size] - np.mean(new_arr[i : i + window_size])))
+			if denom != 0.0:
+					out_arr[i] = num / denom
+	
+	return out_arr
+
+@nb.jit("(f8[:])(f8[:], f8[:], i8, i8)", nopython=True, nogil=True)
+def nb_calc_sentiment_score_a(sent_a, sent_b, ra_win_size, std_win_size):
+	# example method for creating a stationary sentiment score based on Augmento data
+	
+	# compare the raw sentiment values
+	sent_ratio = nb_safe_divide(sent_a, sent_b)
+	
+	# smooth the sentiment ratio
+	sent_ratio_smooth = nb_causal_rolling_average(sent_ratio, ra_win_size)
+	
+	# create a stationary(ish) representation of the smoothed sentiment ratio
+	sent_score = nb_causal_rolling_sd(sent_ratio_smooth, std_win_size)
+	
+	return sent_score
+
+@nb.jit("(f8[:])(f8[:], f8[:], i8, i8)", nopython=True, nogil=True)
+def nb_calc_sentiment_score_b(sent_a, sent_b, ra_win_size_short, ra_win_size_long):
+	# example method for creating a stationary sentiment score based on Augmento data
+	
+	# compare the raw sentiment values
+	sent_ratio = nb_safe_divide(sent_a, sent_b)
+	
+	# smooth the sentiment ratio
+	sent_ratio_short = nb_causal_rolling_average(sent_ratio, ra_win_size_short)
+	sent_ratio_long = nb_causal_rolling_average(sent_ratio, ra_win_size_long)
+	
+	# create a stationary(ish) representation of the smoothed sentiment ratio
+	sent_score = sent_ratio_short - sent_ratio_long
+	
+	return sent_score
+
+@nb.jit("(f8[:])(f8[:], f8[:], i8, i8)", nopython=True, nogil=True)
+def nb_calc_sentiment_score_c(sent_a, sent_b, ra_win_size, std_win_size):
+	# example method for creating a stationary sentiment score based on Augmento data
+	
+	# compare the raw sentiment values
+	sent_ratio = nb_safe_divide(sent_a, sent_b)
+	
+	# smooth the sentiment ratio
+	sent_ratio_smooth = nb_causal_rolling_average(sent_ratio, ra_win_size)
+	
+	# create a stationary(ish) representation of the smoothed sentiment ratio
+	sent_score = nb_causal_rolling_norm(sent_ratio_smooth, std_win_size)
+	
+	return sent_score
+
+@nb.jit("(f8[:])(f8[:], f8[:], f8, f8)", nopython=True, nogil=True, cache=True)
+def nb_backtest_a(price, sent_score, start_pnl, buy_sell_fee):
+	# example backtest with approximate model for long/short contracts
+	
+	# create an array to hold our pnl, and set the first value
+	pnl = np.zeros(price.shape, dtype=np.float64)
+	pnl[0] = start_pnl
+	
+	# for each step, run the market model
+	for i_p in range(1, price.shape[0]):
+		
+		# if sentiment score is positive, simulate long position
+		# else if sentiment score is negative, simulate short position
+		# else if the sentiment score is 0.0, hold
+		# (note that this is a very approximate market simulation!)
+		if sent_score[i_p-1] > 0.0:
+			pnl[i_p] = (price[i_p] / price[i_p-1]) * pnl[i_p-1]
+		elif sent_score[i_p-1] <= 0.0:
+			pnl[i_p] = (price[i_p-1] / price[i_p]) * pnl[i_p-1]
+		elif sent_score[i_p-1] == 0.0:
+			pnl[i_p] = pnl[i_p-1]
+		
+		# simulate a trade fee if we cross from long to short, or visa versa
+		if i_p > 1 and np.sign(sent_score[i_p-1]) != np.sign(sent_score[i_p-2]):
+			pnl[i_p] = pnl[i_p] - (buy_sell_fee * pnl[i_p])
+	
+	return pnl
+
+
+
+
+@nb.jit("(f8[:])(f8[:], i8)", nopython=True, nogil=True, cache=True)
+def moving_average(arr, window):
+        
+        # output array
+	ma_arr = np.zeros(arr.shape[0])
+        
+        # add space for rolling window
+	new_arr = np.hstack((np.ones(window-1) * arr[0], arr))
+        
+        # calculate moving average
+	for i in nb.prange(arr.shape[0]):
+		num = new_arr[i+window-1] - np.mean(new_arr[i : i+window-1])
+		denom = np.std(new_arr[i : i + window-1])
+		if denom != 0.0:
+                        ma_arr[i] = num / denom
+
+	return ma_arr 
+
+
+@nb.jit("(f8[:])(f8[:], f8[:], f8[:], f8, f8, f8)",nopython=True, nogil=True,cache=True)
+def sma_crossover_backtest(price, leading_arr, lagging_arr, start_pnl, buy_sell_fee, threshold=0.0):
+
+	# create an array to hold our pnl, and set the first value
+	pnl = np.zeros(price.shape, dtype=np.float64)
+	pnl[0] = start_pnl
+
+	# BUY if Leading SMA is above Lagging SMA by some threshold.
+	# SELL if Leading SMA is below Lagging SMA by some threshold.
+	sent_signal = leading_arr - lagging_arr
+	
+	# for each step, run the market model
+	for i_p in range(1, price.shape[0]):
+			if sent_signal[i_p-1] > threshold:
+					pnl[i_p] = (price[i_p] / price[i_p-1]) * pnl[i_p-1]
+			elif sent_signal[i_p-1] < threshold:
+					pnl[i_p] = (price[i_p-1] / price[i_p]) * pnl[i_p-1]
+			elif sent_signal[i_p-1] == threshold:
+				pnl[i_p] = pnl[i_p-1]
+	
+	# simulate a trade fee if we cross from long to short, or visa versa
+	if i_p > 1 and np.sign(sent_signal[i_p-1]) != np.sign(sent_signal[i_p-2]):
+		pnl[i_p] = pnl[i_p] - (buy_sell_fee * pnl[i_p])
+	
+	return pnl
+
